c31846c2123a404612973b4f1c7aca8039e35419
lamproskarach <labros.karachristos@novidea.gr>
2025-10-13 12:51:07 +0300

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import { ThemeProvider } from "./components/ThemeProvider";
 import RocketScrollIndicator from "./components/RocketScrollIndicator";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
           <Footer />
           <RocketScrollIndicator />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
